7c748363622c4996e28f0fc11cc03e95f8dc4bdf
Ellen Sundh <ellen.sundh@gmail.com>
2012-04-15 18:39:03 +0200

    Example of Bocki Message Board European characters

---
+++ examples/EuropeanCharachters/EuropeanCharachters.ino
@@ -0,0 +1,24 @@
+
+#include <BockiMessageBoard.h>
+
+BockiMessageBoard messageBoard = BockiMessageBoard(0); // Pass in ID of message board
+
+
+void setup()
+{
+  Serial.begin(9600);
+  int displayTime = 3; // Input 0.5 sec or 1 to 25 seconds
+  
+  //Animation types: IMMEDIATE, CURTAINUP, CURTAINDOWN, SCROLLLEFT, SCROLLRIGHT, VERTICALOPEN, VERTICALCLOSE, SCROLLUP, SCROLLDOWN, HOLD, SNOW, TWINKLE, BLOCKMOVE
+  messageBoard.configure(displayTime,SCROLLLEFT,SCROLLLEFT);
+}
+
+void loop()
+{
+  // Testing with Swedish characters ÅÄÖåäö
+  String euroChars = "Testing with Swedish characters <U45><U44><U76><U65><U64><U76>";
+  messageBoard.displayMessage(PAGE_A,euroChars); // Page id PAGE_A - PAGE_Z and message of your choice
+  delay(10000);
+  
+}
+
